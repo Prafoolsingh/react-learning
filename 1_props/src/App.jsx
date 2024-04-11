ce8f34aa43ba127a153e3edaf5ccc383eab6c2ba
prafool singh <singhpraful61@gmail.com>
2024-04-11 17:58:14 +0530

    props

---
+++ 1_props/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+  let myObj = {
+    username: "hitesh",
+    age: 21
+  }
+  let newArr = [1, 2, 3]
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
+
+      <Card username="Prafool singh kafaliya" btnText="click me" />
+      <Card username="Kamal" />
+    </>
+  )
+}
+
+export default App
